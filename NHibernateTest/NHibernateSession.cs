65c4390586b0990b476a5dbda912548b71e5d5b9
Etienne Morin <etienne.morin@boldradius.com>
2014-07-29 09:26:29 -0400

    Initial commit of working solution

---
+++ NHibernateTest/NHibernateSession.cs
@@ -0,0 +1,15 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+
+namespace NHibernateTest
+{
+    public class NHibernateSession
+    {
+        public static ISession OpenSession()
+        {
+            var sessionFactory = new Configuration().Configure().BuildSessionFactory();
+            return sessionFactory.OpenSession();
+        }
+    }
+
+}
---
+++ NHibernateTest/NHibernateTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Linq;
+
+namespace NHibernateTest
+{
+    [TestClass]
+    public class NHibernateTest
+    {
+        [TestMethod]
+        public void SaveCat()
+        {
+            using (ISession session = NHibernateSession.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    var princess = new Cat {Name = "Princess", Sex = 'F', Weight = new Random(10).Next() / 1.0f};
+                    session.Save(princess);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        [TestMethod]
+        public void GetCats()
+        {
+            using (ISession session = NHibernateSession.OpenSession())
+            {
+                var cats = session.Query<Cat>().ToList();
+                Assert.IsTrue(cats.Count > 0);
+            }
+        }
+    }
+}
